d506bf4788847fc038bc61f158966b933a2bd52f
ynonp <ynonperek@gmail.com>
2011-06-30 11:31:24 +0300

    add bash example

--- sol5/ex4.sh
+++ sol5/ex4.sh
@@ -3,7 +3,12 @@
 # ex4.sh
 #
 
-[[ -r $1 ]] && echo "$1 is readable"
-[[ -w $1 ]] && echo "$1 is writable"
-[[ -x $1 ]] && echo "$1 is executable"
+if [[ ! $# == 1 ]]; then
+    echo "Usage: $0 <pathname>"
+    exit 1
+fi
+
+[ -r $1 ] && echo "$1 is readable"
+[ -w $1 ] && echo "$1 is writable"
+[ -x $1 ] && echo "$1 is executable"
 
